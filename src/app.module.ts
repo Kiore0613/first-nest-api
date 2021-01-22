0306c0c18adfab88ae999ade0b3031a2cce2d7c7
kiore <ricardochicas95@gmail.com>
2021-01-22 01:58:49 -0600

    refactor files

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,20 +7,7 @@ import { User } from './entity/user.entity';
 import { UserService } from './service/user/user.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'carebook-db-mysql-do-user-8598233-0.b.db.ondigitalocean.com',
-      port: 25060,
-      logging: true,
-      username: 'kiore',
-      password: 'v781qrp5019obwwc',
-      database: 'defaultdb',
-      entities: ['dist/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
-    TypeOrmModule.forFeature([User]),
-  ],
+  imports: [TypeOrmModule.forRoot(), TypeOrmModule.forFeature([User])],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
