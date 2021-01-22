a69c9c2669099ca94ee57cc5dd9e66bcca7451d6
kiore <ricardochicas95@gmail.com>
2021-01-22 01:44:32 -0600

    Add imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,21 @@ import { UserController } from './controller/user/user.controller';
 import { User } from './entity/user.entity';
 import { UserService } from './service/user/user.service';
 
-
-
 @Module({
-  imports: [TypeOrmModule.forRoot(), TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'carebook-db-mysql-do-user-8598233-0.b.db.ondigitalocean.com',
+      port: 25060,
+      logging: true,
+      username: 'kiore',
+      password: 'v781qrp5019obwwc',
+      database: 'defaultdb',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
